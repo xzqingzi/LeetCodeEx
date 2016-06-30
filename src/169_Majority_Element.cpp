c06f68996b6b84c7130910a878647619116dced2
tfeng <tfeng@ucsd.edu>
2016-06-30 15:38:48 -0700

    06_30

--- src/169_Majority_Element.cpp
+++ src/169_Majority_Element.cpp
@@ -41,3 +41,13 @@ public:
         return maj;
     }
 };
+
+// Other
+class Solution {
+public:
+int majorityElement(vector<int>& nums) {
+    int size=nums.size();
+    nth_element(nums.begin(),nums.begin()+size/2,nums.end());
+    return nums[size/2];
+}
+};
