f3d227cf16a4c5f75e7408f9e7c57e2d32cba50a
tfeng <tfeng@ucsd.edu>
2016-07-11 22:14:59 -0700

    07_10

--- src/349_Intersection_of_Two_Arrays.cpp
+++ src/349_Intersection_of_Two_Arrays.cpp
@@ -12,3 +12,47 @@ public:
         return outlist;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0, j=0;
+        vector<int> outlist;
+        while(i< nums1.size() && j<nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            }
+            else if (nums1[i] > nums2[j]) {
+                j++;
+            }
+            else {
+                if ( !outlist.size() || nums1[i] != outlist.back()) {  
+                    // Can NOT be (nums1[i] != outlist.back()||!outlist.size()) , cause runtime error, WHY?
+                    outlist.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        return outlist;
+    }
+};
+
+
+//  Others code 
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> m(nums1.begin(), nums1.end());
+        vector<int> res;
+        for (auto a : nums2) //means loop the vector nums based on iterator, since use auto a can be any type of values
+            if (m.count(a)) {
+                res.push_back(a);
+                m.erase(a);
+            }
+        return res;
+    }
+};
--- src/58_Length_of_Last_Word.cpp
+++ src/58_Length_of_Last_Word.cpp
@@ -10,3 +10,36 @@ public:
         return count;
     }
 };
+
+
+// Other build in func solution 
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        while(s.back()==' ') s.erase(s.length()-1); //while last pos are ' ', remove them , until the last pos is digit
+        return s.length()-s.rfind(' ')-1; // rfind() gives the last pos of ' '
+    }
+};
+
+
+//Others solution
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len=0;
+        int pos;
+        
+        while (s.length()!=0){
+            pos = s.find(" ");
+            if (pos==string::npos)  // Used in string, means "until the end of the string".
+                return s.length();
+            else{
+                if (pos!=0)
+                    len = pos;
+                s.erase(0,pos+1);
+            }
+        }
+        return len;
+    }
+};
